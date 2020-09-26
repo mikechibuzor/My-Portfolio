568b1e0c646ca131aa78c189e871fe0d85444ff6
__Chi__ <iwuagwuchibuzor@gmail.com>
2020-09-26 13:52:32 +0100

    finished the basic structure for home-page

--- Scripts/Utility/HomePage.js
+++ Scripts/Utility/HomePage.js
@@ -3,17 +3,70 @@ class HomePage extends HTMLElement {
     super();
     this._checkAttrChange = checkAttrChange;
     this.attachShadow({ mode: "open" });
-    this.shadowRoot.innerHTML = `
-        <div class="home components">
-          <p>${this._portfolioOwner}</p>
-        </div>
-    `;
   }
 
   connectedCallback() {
     this._portfolioOwner = this.getAttribute("name") || "name goes here";
     this._logo = this.getAttribute("logo") || "logo";
     this._jobs = this.getAttribute("jobs") || ["Job 1", "Job2", "Job3"];
+
+    this.shadowRoot.innerHTML = `
+        <style>
+          *{
+            padding: 0;
+            margin: 0;
+            box-sizing: border-box;
+          }
+          .home{
+            height: 100vh;
+          }
+
+          .home .content{
+            border: 1px solid black;
+            height: 100%;
+            display: flex;
+          }
+
+          .home .content .menu{
+            flex: 0 0 30%;
+            border: 1px solid green;
+            height: 100%;            
+          }
+
+          .home .content .cont{
+            flex: 0 0 70%;
+            border: 1px solid yellow;
+            height: 100%;
+            display: flex;
+            flex-wrap: wrap;
+          }
+
+          .home .content .cont .contText{
+            flex: 0 0 100%;
+            height: 85%;
+            border: 1px solid pink;
+          }
+
+          .home .content .cont .footer{
+            flex: 0 0 100%;
+            border: 1px solid blue;
+            height: 15%;
+          }
+        </style>
+        <div class="home components">
+          <div class="content">
+            <div class="menu">
+            </div>
+            <div class="cont">
+              <div class="contText">
+              </div>
+              <div class="footer">
+              </div>
+            </div>
+            
+          </div>
+        </div>
+    `;
   }
 
   attributeChangedCallback(name, oldValue, newValue) {}
